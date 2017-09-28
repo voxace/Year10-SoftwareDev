cfa026b78a02ef206cc52955aaa1dc581007bde5
voxace <dave@voxace.com>
2017-09-28 16:17:00 +1000

    Created NumbnerFun program.

---
+++ NumberFun/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inputs
+            Console.WriteLine("Please enter a number for X:");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a number for Y:");
+            float y = float.Parse(Console.ReadLine());
+
+            // Output
+            Console.WriteLine("X plus Y = " + (x + y));
+            Console.WriteLine("X times Y = " + (x * y));
+            Console.WriteLine("X minus Y = " + (x - y));
+            Console.WriteLine("X divided by Y = " + (x / y));
+
+            // Wait
+            Console.ReadLine();
+        }
+    }
+}
