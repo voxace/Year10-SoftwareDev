17492ac20b861346b2e7a0a83945af4ea4908224
voxace <dave@voxace.com>
2017-09-29 09:54:04 +1000

    Created the AgeProgram!

---
+++ AgeProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AgeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            Console.WriteLine("Please enter your age:");
+            DateTime birthday = DateTime.Parse(Console.ReadLine());
+
+            // Calculation
+            int age = DateTime.Today.Year - birthday.Year;
+
+            // Selection
+            if (age >= 0 && age < 13)
+            {
+                Console.WriteLine("You are a child!");
+            }
+            else if (age >= 13 && age < 18)
+            {
+                Console.WriteLine("You are a teenager!");
+            }
+            else if(age >= 18)
+            {
+                Console.WriteLine("You are an adult!");
+            }
+            else
+            {
+                Console.WriteLine("You are not born yet!");
+            }
+
+            // Wait
+            Console.ReadLine();
+        }
+    }
+}
